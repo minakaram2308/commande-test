31dc9eff39a9d6f6e5d3bc0b1d53988a06264903
minakaram12 <minakaramcs@gmail.com>
2024-08-26 02:32:57 +0300

    updates

--- resources/react/layout.jsx
+++ resources/react/layout.jsx
@@ -3,12 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/app.css";
 import CommandePartner from "./pages/CommandePartner";
 import GroceryPartner from "./pages/GroceryPartner";
+import RestaurantPartner from "./pages/RestaurantPartner";
+import FoodDelivery from "./pages/FoodDelivery";
+import SalesWithPickup from "./pages/SalesWithPickup";
 
 function layout() {
     return (
         <div className="layout">
+            {/* 1 */}
             {/* <CommandePartner /> */}
-            <GroceryPartner />
+            {/* 2 */}
+            {/* <GroceryPartner /> */}
+            {/* 3 */}
+            {/* <RestaurantPartner /> */}
+            {/* 4 */}
+            {/* <FoodDelivery /> */}
+            {/* 5 */}
+            <SalesWithPickup />
+            {/* 6 */}
+            {/* 7 */}
+            {/* 8 */}
+            {/* 9 */}
+            {/* 10 */}
+            {/* 11 */}
+            {/* 12 */}
+            {/* 13 */}
+            {/* 14 */}
+
+           
         </div>
     );
 }
